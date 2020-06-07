ac7921a8877e43a0051decdb5b505cfcdbd8d9e1
ruben medina <ruben@megachopp.com.py>
2020-06-07 19:44:18 -0400

    probando megal

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,44 @@
+require 'json'
+require 'open-uri'
+
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
+    url = 'https://walkin-app.com/v2.0/habitaciones/habitacionesOcupadas/api/ocupado'
+    user_serialized = open(url).read
+    user = JSON.parse(user_serialized)
+    @hab = user.first[1].length
+
+    url = 'https://walkin-app.com/v2.0/habitaciones/habitacionesOcupadas/api/mantenimiento'
+    user_serialized = open(url).read
+    user = JSON.parse(user_serialized)
+    @hab_mantenimiento = user.first[1].length
+
+    url = 'https://walkin-app.com/v2.0/habitaciones/habitacionesOcupadas/api/disponible'
+    user_serialized = open(url).read
+    user = JSON.parse(user_serialized)
+    @hab_disponible = user.first[1].length
+  end
+
+  def ocupados
+    url = 'https://walkin-app.com/v2.0/habitaciones/habitacionesOcupadas/api/ocupado'
+    user_serialized = open(url).read
+    user = JSON.parse(user_serialized)
+    @hab = user.first[1]
+  end
+
+  def disponibles
+    url = 'https://walkin-app.com/v2.0/habitaciones/habitacionesOcupadas/api/disponible'
+    user_serialized = open(url).read
+    user = JSON.parse(user_serialized)
+    @hab = user.first[1]
+  end
+
+  def mantenimientos
+    url = 'https://walkin-app.com/v2.0/habitaciones/habitacionesOcupadas/api/mantenimiento'
+    user_serialized = open(url).read
+    user = JSON.parse(user_serialized)
+    @hab = user.first[1]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "ocupados", to: 'pages#ocupados'
+  get "mantenimientos", to: 'pages#mantenimientos'
+  get "disponibles", to: 'pages#disponibles'
+  get "limpiezas", to: 'pages#limpiezas'
 end
